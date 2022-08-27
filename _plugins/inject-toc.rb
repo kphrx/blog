541fe8f8f4e9146e81109d931d4d752800478767
kPherox <admin@mail.kr-kp.com>
2022-08-27 20:44:12 +0900

    refactor: remove duplicated code and unused alias

--- _plugins/inject-toc.rb
+++ _plugins/inject-toc.rb
@@ -1,8 +1,11 @@
 Doc = Kramdown::Document
-Elm = Kramdown::Element
 
-def is_markdown?(ext)
-  [".markdown", ".mkdown", ".mkdn", ".mkd", ".md"].include?(ext)
+def is_page_or_post?(class_name)
+  ["Jekyll::Page", "Jekyll::Document"].include?(class_name)
+end
+
+def is_markdown?(extname)
+  [".markdown", ".mkdown", ".mkdn", ".mkd", ".md"].include?(extname)
 end
 
 def disabled?(data)
@@ -13,23 +16,11 @@ def insert_toc(content, level, text)
   prefix = "#" * level
   header_reg = Regexp.new(prefix + " *?" + Regexp.escape(text))
 
-  return content.gsub(header_reg, "{% include toc.md %}\n\n" + prefix + " " + text)
+  content.gsub(header_reg, "{% include toc.md %}\n\n" + prefix + " " + text)
 end
 
-Jekyll::Hooks.register :pages, :pre_render do |page|
-  if page.class.name == "Jekyll::Page" and is_markdown?(page.extname) and not disabled?(page.data)
-    doc = Doc.new(page.content)
-    children = doc.root.children
-    first_header = children.index {|element| element.type == :header }
-    unless first_header.nil?
-      first_header = children.fetch(first_index)
-      page.content = insert_toc(page.content, first_header.options[:level], first_header.options[:raw_text])
-    end
-  end
-end
-
-Jekyll::Hooks.register :documents, :pre_render do |page, payload|
-  if is_markdown?(page.extname) and not disabled?(page.data)
+Jekyll::Hooks.register [:pages, :documents], :pre_render do |page|
+  if is_page_or_post?(page.class.name) and is_markdown?(page.extname) and not disabled?(page.data)
     doc = Doc.new(page.content)
     children = doc.root.children
     first_index = children.index {|element| element.type == :header }
