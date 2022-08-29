358c6beb15b538398557f40ecbb56af20fc0c603
kPherox <admin@mail.kr-kp.com>
2022-08-29 23:40:28 +0900

    refactor: patch kramdown syntax highlighter rouge

--- _plugins/kramdown-syntax-highlighter-rouge-patch.rb
+++ _plugins/kramdown-syntax-highlighter-rouge-patch.rb
@@ -1,54 +1,57 @@
 require 'kramdown/converter/syntax_highlighter/rouge'
 
-module RougeFormatFix
+module KramdownSyntaxHighlighterFix
   def call(converter, text, lang, type, call_opts)
     opts = options(converter, type)
     call_opts[:default_lang] = opts[:default_lang]
     return nil unless lang || opts[:default_lang] || opts[:guess_lang]
 
     lexer = Rouge::Lexer.find_fancy(lang || opts[:default_lang], text)
+    return nil if !lexer && !lang
     return call(converter, text, nil, type, call_opts) unless lexer
     return nil if opts[:disable] || (lexer.tag == "plaintext" && !opts[:guess_lang])
 
-    opts[:css_class] ||= 'highlight' # For backward compatibility when using Rouge 2.0
-    formatter = formatter_class(opts)
+    opts[:css_class] ||= 'highlight'
+    formatter = new_formatter(formatter_class(opts), opts) # initialize 呼び出しを包む
+    if opts[:wrap] && !formatter.is_a?(Rouge::Formatters::HTMLPygments)
+      formatter = Rouge::Formatters::HTMLPygments.new(formatter, opts.fetch(:css_class, 'codehilite'))
+    end
     formatter.format(lexer.lex(text))
   end
 
-  def formatter_class(opts = {})
-    formatter = super(opts)
+  # 既知の Rouge:Formatters の initialize 引数の形式で出し分ける
+  def new_formatter(formatter, opts)
     case
-    when ["Rouge::Formatter",
-          "Rouge::Formatters::HTML",
-          "Rouge::Formatters::HTMLLegacy",
-          "Rouge::Formatters::Tex",
-          "Rouge::Formatters::Null"].include?(formatter.to_s)
-      formatter.new(opts)
+    when "Rouge::Formatters::HTMLInline" === formatter.to_s
+      formatter.new(opts.fetch([:inline_theme], 'github'))
+
+    when ["Rouge::Formatters::Terminal256",
+          "Rouge::Formatters::TerminalTruecolor"].include?(formatter.to_s)
+      opts[:theme] ? formatter.new(opts[:theme]) : formatter.new()
+
+    when "Rouge::Formatters::HTMLPygments" === formatter.to_s
+      formatter.new(base_formatter(opts), opts.fetch(:css_class, 'codehilite'))
 
     when ["Rouge::Formatters::HTMLTable",
           "Rouge::Formatters::HTMLLinewise",
           "Rouge::Formatters::HTMLLineTable",
           "Rouge::Formatters::HTMLLineHighlighter"].include?(formatter.to_s)
-      base_opts = opts.dup
-      base_opts[:formatter] = base_opts.delete(:base_formatter) || Rouge::Formatters::HTML
-      base_formatter = formatter_class(base_opts)
-      formatter.new(base_formatter, opts)
-
-    when ["Rouge::Formatters::HTMLInline",
-          "Rouge::Formatters::Terminal256",
-          "Rouge::Formatters::TerminalTruecolor"].include?(formatter.to_s)
-      formatter.new(opts[:theme])
-
-    when "Rouge::Formatters::HTMLPygments" === formatter.to_s
-      base_opts = opts.dup
-      base_opts[:formatter] = base_opts.delete(:base_formatter) || Rouge::Formatters::HTML
-      base_formatter = formatter_class(base_opts)
-      formatter.new(base_formatter, opts[:css_class])
+      formatter.new(base_formatter(opts), opts)
 
     else
+      # Rouge::Formatter
+      # Rouge::Formatters::HTML
+      # Rouge::Formatters::HTMLLegacy
+      # Rouge::Formatters::Tex
+      # Rouge::Formatters::Null
       formatter.new(opts)
     end
   end
+
+  def base_formatter(opts)
+    opts[:inline_theme] ? Rouge::Formatters::HTMLInline.new(opts[:inline_theme])
+                        : Rouge::Formatters::HTML.new
+  end
 end
 
-Kramdown::Converter::SyntaxHighlighter::Rouge.singleton_class.prepend(RougeFormatFix)
+Kramdown::Converter::SyntaxHighlighter::Rouge.singleton_class.prepend(KramdownSyntaxHighlighterFix)
